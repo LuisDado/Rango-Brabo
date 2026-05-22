491541453eae67a48529f7bc1bf6307d1d81a858
LuisHack <4553686090@estudante.sed.sc.gov.br>
2026-05-22 08:40:30 -0300

    Add files via upload

---
+++ NavBar.jsx
@@ -0,0 +1,25 @@
+function Navbar2(){
+  return(
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid">
+
+        <a className="navbar-brand" href="#">
+
+         
+          <img
+            src="https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg"
+            alt="Logo"
+            width="30"
+            height="24"
+            className="d-inline-block align-text-top"
+          />
+
+          Bootstrap
+        </a>
+
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar2;
