a4f009c51a9a1509838ebab2f9f38ef6cf65ecf4
LuisHack <4553686090@estudante.sed.sc.gov.br>
2026-05-22 08:07:52 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,48 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar2 from './components/Nav';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className='colors'><Navbar2/></div>
+    <div className="caixa">
+      <img src="https://br.pinterest.com/pin/689754499215899638/" alt="Imagem lateral loja"/>
+      <h1>Rango Brabo</h1>
+      <p>Na Rango Brabo, cada lanche é feito pra quem curte sabor de verdade. Inspirada na cultura de rua, a hamburgueria mistura atitude, hambúrguer artesanal suculento, ingredientes pesados e aquele clima urbano que transforma qualquer fome em experiência. Aqui não tem gourmetização sem graça — tem smash na chapa, cheddar escorrendo, bacon crocante e combos feitos pra matar a larica sem dó. </p>
+    </div>
+    <div className='caixa'>
+      <h1>Cardápio</h1>
+      <p>BURGERS BRABO
+Quebra Queixo — R$ 22
+
+Pão brioche, smash bovino, cheddar derretido e molho especial.
+
+Favela Cheese — R$ 26
+
+Dois smashs, cheddar duplo, cebola caramelizada e bacon crocante.
+
+Brabo Picante — R$ 28
+
+Hambúrguer artesanal, queijo prato, jalapeño, molho apimentado e onion rings.
+
+Monstro da Rua — R$ 34
+
+Três carnes, cheddar triplo, bacon, cebola crispy e molho especial da casa.
+
+BEBIDAS
+Refrigerante Lata — R$ 6
+Copo Refil — R$ 10
+Milkshake Brabíssimo — R$ 16
+
+Sabores disponíveis:
+• Chocolate
+• Morango
+• Ovomaltine</p>
+<img src="https://br.pinterest.com/pin/689754499215899638/" alt="Imagem lateral loja"/>
+    </div>
+    </>
+  );
+}
+
+export default App;
